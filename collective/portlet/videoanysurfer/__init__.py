a30359ba438a8afa2c90d89a9c0859ff8c34604f
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-06-26 17:19:05 +0200

    initial commit

---
+++ collective/portlet/videoanysurfer/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+VideoPortletMessageFactory = MessageFactory('collective.portlet.videoanysurfer')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
